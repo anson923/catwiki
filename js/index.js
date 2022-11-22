375886b3c4190ad5347a72a83d1134e1def262fb
AnsonDev <65697201+anson923@users.noreply.github.com>
2022-11-21 21:00:50 -0800

    added load data from api/local and import data into template

--- js/index.js
+++ js/index.js
@@ -1,3 +1,53 @@
+$( document ).ready(function() {
+  //Wait until cat data and country data is loaded.
+  (async() => {
+
+    while(catData.data === null && countryData.data === null)
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log("fetch successfully!");
+  })();
+
+  if ('content' in document.createElement('template'))
+  {
+    const gridContent = document.getElementById('cat-preview-grid');
+    for(const cat of catData.data)
+    {
+      const template = document.querySelector('#cat-preview-template');
+      const clone = template.content.cloneNode(true);
+
+      console.warn("Cat object",cat)
+
+      let cat_image = clone.querySelector('#cat-image');
+      cat_image.src = cat.image === undefined ? '/resources/img/pageLogo.png' : cat.image.url;
+      
+
+      let cat_name = clone.querySelector('#cat-name');
+      cat_name.innerText = cat.name;
+
+      let cat_flag = clone.querySelector('#cat-flag');
+      let imgSrc = countryData.data.find(x=> x.alpha2Code === cat.country_code).flag;
+      console.log(imgSrc)
+      cat_flag.src = imgSrc === null ? '' : imgSrc;
+
+      let cat_country = clone.querySelector('#cat-country');
+      cat_country.innerText = cat.origin;
+
+      let cat_description = clone.querySelector('#cat-description');
+      cat_description.innerText = cat.description;
+
+      let cat_temperament = clone.querySelector('#cat-temperament')
+      cat_temperament.innerText = cat.temperament
+
+      gridContent.appendChild(clone);
+    }
+    
+    
+  }
+  else{
+    console.error("Current Browser is not supporting Template!");
+  }
+});
+
 const catData = {
   url:'https://api.thecatapi.com/v1/breeds',
   key:'catdata_v1',
@@ -15,7 +65,7 @@ const tryFetchDataFromURL = async (objData) => {
     console.log('%c Fetching Data...', 'color:red')
     const response = await fetch(objData.url);
     const data = await response.json();
-    console.table(data);
+    // console.table(data);
 
     setLocalData(objData,JSON.stringify(data));
     
@@ -27,6 +77,7 @@ const tryFetchDataFromURL = async (objData) => {
   }
 }
 
+//Model
 function setLocalData(objData, newData)
 {
   objData.data = newData;
@@ -41,6 +92,7 @@ function getLocalData(key){
   return null;
 }
 
+//Controller
 function FetchData(objData)
 {
   objData.data = JSON.parse(getLocalData(objData.key));
@@ -50,6 +102,7 @@ function FetchData(objData)
     tryFetchDataFromURL(objData);
   }
 }
-
+console.log("fetching for data...");
 FetchData(catData);
 FetchData(countryData);
+
