7acbad1af1617363c8cda80ee1840489d3314d22
AnsonDev <65697201+anson923@users.noreply.github.com>
2022-11-21 19:25:41 -0800

    Fetch data from cat api, country api and save/load local storage

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
-const catUrl = 'https://api.thecatapi.com/v1/breeds';
-const flagUrl = 'https://restcountries.com/v2/all';
+const catData = {
+  url:'https://api.thecatapi.com/v1/breeds',
+  key:'catdata_v1',
+  data: null
+};
 
-const fetchCountryData = async () => {
+const countryData = {
+  url:'https://restcountries.com/v2/all',
+  key:'countrydata_v2',
+  data: null
+};
+
+const tryFetchDataFromURL = async (objData) => {
   try{
     console.log('%c Fetching Data...', 'color:red')
-    const response = await fetch(url);
+    const response = await fetch(objData.url);
     const data = await response.json();
-    console.table(data[0]);
+    console.table(data);
+
+    setLocalData(objData,JSON.stringify(data));
+    
     console.log('%c Success','color:green')
   }
   catch(err)
@@ -15,4 +27,29 @@ const fetchCountryData = async () => {
   }
 }
 
- //fetchCountryData();
+function setLocalData(objData, newData)
+{
+  objData.data = newData;
+  window.localStorage.setItem(objData.key, newData);
+}
+
+function getLocalData(key){
+  if(window.localStorage.getItem(key) !== null)
+  {
+    return window.localStorage.getItem(key);
+  }
+  return null;
+}
+
+function FetchData(objData)
+{
+  objData.data = JSON.parse(getLocalData(objData.key));
+  
+  if(objData.data === null)
+  {
+    tryFetchDataFromURL(objData);
+  }
+}
+
+FetchData(catData);
+FetchData(countryData);
