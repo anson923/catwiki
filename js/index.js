9ea0e34d32a86005ff8c5447c45238c047e0819c
AnsonDev <65697201+anson923@users.noreply.github.com>
2022-11-21 18:10:04 -0800

    added first sample cat-preview

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
-const url = 'https://api.thecatapi.com/v1/breeds';
+const catUrl = 'https://api.thecatapi.com/v1/breeds';
+const flagUrl = 'https://restcountries.com/v2/all';
 
 const fetchCountryData = async () => {
   try{
     console.log('%c Fetching Data...', 'color:red')
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+    console.table(data[0]);
     console.log('%c Success','color:green')
   }
   catch(err)
@@ -14,4 +15,4 @@ const fetchCountryData = async () => {
   }
 }
 
-fetchCountryData();
+ //fetchCountryData();
